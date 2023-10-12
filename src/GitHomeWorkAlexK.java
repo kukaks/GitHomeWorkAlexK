6b13947533ee08540792fbd3fdf41fb6747392e3
alexander.kudlay <alexander.kudlay@x2sy.com>
2023-10-12 23:24:37 +0300

    Second commit

--- src/GitHomeWorkAlexK.java
+++ src/GitHomeWorkAlexK.java
@@ -1,5 +1,6 @@
 public class GitHomeWorkAlexK {
     public static void main(String[] args){
         System.out.println("Home Work Git. Alex K");
+        System.out.println("Home Work Git. Second commit. Alex K");
     }
 }
